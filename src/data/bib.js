dcd970e7ed23874f27627926d97334164ba14dbb
Bhanu-Dev <bhanuthapa252@gmail.com>
2026-05-12 15:40:33 +0530

    first commit

---
+++ src/data/bib.js
@@ -0,0 +1,148 @@
+﻿var bib = {};
+
+bib["redmon2016yolo"] = {
+    type: "inproceedings",
+    author: "Joseph Redmon and Santosh Divvala and Ross Girshick and Ali Farhadi",
+    title: "You Only Look Once: Unified, Real-Time Object Detection",
+    booktitle: "Proceedings of the IEEE Conference on Computer Vision and Pattern Recognition (CVPR)",
+    series: "CVPR",
+    keywords: "yolo object_detection computer_vision cnn deep_learning real_time_detection",
+    abstract: "Introduced the YOLO framework for unified real-time object detection using a single neural network.",
+    pages: "779--788",
+    year: "2016",
+    doi: "10.1109/CVPR.2016.91"
+};
+
+bib["redmon2017yolo9000"] = {
+    type: "inproceedings",
+    author: "Joseph Redmon and Ali Farhadi",
+    title: "YOLO9000: Better, Faster, Stronger",
+    booktitle: "Proceedings of the IEEE Conference on Computer Vision and Pattern Recognition (CVPR)",
+    series: "CVPR",
+    keywords: "yolo9000 object_detection computer_vision deep_learning real_time_detection",
+    abstract: "Improved YOLO with joint training and hierarchical classification for detecting over 9000 object categories.",
+    pages: "7263--7271",
+    year: "2017",
+    doi: "10.1109/CVPR.2017.690"
+};
+
+bib["redmon2018yolov3"] = {
+    type: "article",
+    author: "Joseph Redmon and Ali Farhadi",
+    title: "YOLOv3: An Incremental Improvement",
+    journal: "arXiv preprint arXiv:1804.02767",
+    series: "arXiv",
+    keywords: "yolov3 object_detection darknet deep_learning cnn",
+    abstract: "Presented incremental improvements to YOLO including multi-scale predictions and better backbone architecture.",
+    year: "2018",
+    doi: "10.48550/arXiv.1804.02767",
+    url: "https://arxiv.org/abs/1804.02767"
+};
+
+bib["bochkovskiy2020yolov4"] = {
+    type: "article",
+    author: "Alexey Bochkovskiy and Chien-Yao Wang and Hong-Yuan Mark Liao",
+    title: "YOLOv4: Optimal Speed and Accuracy of Object Detection",
+    journal: "arXiv preprint arXiv:2004.10934",
+    series: "arXiv",
+    keywords: "yolov4 object_detection bag_of_freebies cnn computer_vision",
+    abstract: "Introduced YOLOv4 with optimized training methods for improved detection speed and accuracy.",
+    year: "2020",
+    doi: "10.48550/arXiv.2004.10934",
+    url: "https://arxiv.org/abs/2004.10934"
+};
+
+bib["jocher2020yolov5"] = {
+    type: "software",
+    author: "Glenn Jocher and Ayush Chaurasia and Jing Qiu",
+    title: "YOLO by Ultralytics",
+    publisher: "Zenodo",
+    series: "Zenodo",
+    keywords: "yolov5 ultralytics object_detection pytorch deep_learning",
+    abstract: "Ultralytics implementation of YOLOv5 for real-time object detection using PyTorch.",
+    version: "5.0",
+    year: "2020",
+    doi: "10.5281/zenodo.3908559",
+    url: "https://github.com/ultralytics/yolov5"
+};
+
+bib["wang2023yolov7"] = {
+    type: "inproceedings",
+    author: "Chien-Yao Wang and Alexey Bochkovskiy and Hong-Yuan Mark Liao",
+    title: "YOLOv7: Trainable Bag-of-Freebies Sets New State-of-the-Art for Real-Time Object Detectors",
+    booktitle: "Proceedings of the IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR)",
+    series: "CVPR",
+    keywords: "yolov7 object_detection realtime_detection computer_vision cnn",
+    abstract: "Presented YOLOv7 with trainable bag-of-freebies achieving state-of-the-art real-time detection performance.",
+    pages: "7464--7475",
+    year: "2023",
+    doi: "10.1109/CVPR52729.2023.00720"
+};
+
+bib["ultralytics2024yolov8"] = {
+    type: "software",
+    author: "Ultralytics",
+    title: "Ultralytics YOLOv8",
+    publisher: "GitHub",
+    series: "GitHub",
+    keywords: "yolov8 ultralytics object_detection segmentation tracking",
+    abstract: "Modern YOLOv8 framework supporting detection, segmentation, classification, and tracking tasks.",
+    year: "2024",
+    url: "https://github.com/ultralytics/ultralytics",
+    note: "Accessed: 2026-05-12"
+};
+
+bib["ge2021yolox"] = {
+    type: "article",
+    author: "Zheng Ge and Songtao Liu and Feng Wang and Zeming Li and Jian Sun",
+    title: "YOLOX: Exceeding YOLO Series in 2021",
+    journal: "arXiv preprint arXiv:2107.08430",
+    series: "arXiv",
+    keywords: "yolox anchor_free object_detection deep_learning cnn",
+    abstract: "Introduced YOLOX, an anchor-free version of YOLO with improved detection performance.",
+    year: "2021",
+    doi: "10.48550/arXiv.2107.08430",
+    url: "https://arxiv.org/abs/2107.08430"
+};
+
+bib["balasubramaniam2022autonomous"] = {
+    type: "article",
+    author: "A. Balasubramaniam and Sudeep Pasricha",
+    title: "Object Detection in Autonomous Vehicles: Status and Open Challenges",
+    journal: "arXiv preprint arXiv:2201.07706",
+    series: "arXiv",
+    keywords: "autonomous_vehicles object_detection computer_vision self_driving",
+    abstract: "Surveyed object detection techniques and open challenges in autonomous driving systems.",
+    year: "2022",
+    doi: "10.48550/arXiv.2201.07706",
+    url: "https://arxiv.org/abs/2201.07706"
+};
+
+bib["sobek2023medyolo"] = {
+    type: "article",
+    author: "Jakub Sobek and Juan R. Medina Inojosa and B. J. Medina Inojosa and S. M. Rassoulinejad-Mousavi and Giuseppe M. Conte and Francisco Lopez-Jimenez and Bradley J. Erickson",
+    title: "MedYOLO: A Medical Image Object Detection Framework",
+    journal: "arXiv preprint arXiv:2312.07729",
+    series: "arXiv",
+    keywords: "medyolo medical_imaging object_detection healthcare deep_learning",
+    abstract: "Presented MedYOLO framework for medical image object detection applications.",
+    year: "2023",
+    doi: "10.48550/arXiv.2312.07729",
+    url: "https://arxiv.org/abs/2312.07729"
+};
+
+bib["xu2024uavyolov8"] = {
+    type: "article",
+    author: "Lin Xu and Yulin Zhao and Yifan Zhai and Lin Huang and Chunyu Ruan",
+    title: "Small Object Detection in UAV Images Based on YOLOv8n",
+    journal: "International Journal of Computational Intelligence Systems",
+    publisher: "Springer",
+    series: "IJCIS",
+    keywords: "uav yolov8 small_object_detection aerial_imaging drone_detection",
+    abstract: "Improved YOLOv8n for detecting small objects in UAV imagery.",
+    volume: "17",
+    number: "1",
+    pages: "223",
+    year: "2024",
+    doi: "10.1007/s44196-024-00632-3"
+};
---
+++ src/data/generated/bib.js
@@ -0,0 +1,164 @@
+﻿define({ entries : {
+
+    "Beck2016Visual": {
+        "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively in information visualization and visual analytics research.",
+        "author": "Beck, Fabian and Koch, Sebastian and Weiskopf, Daniel",
+        "doi": "10.1109/TVCG.2015.2467757",
+        "journal": "IEEE Transactions on Visualization and Computer Graphics",
+        "keywords": "type:system, visual_analytics",
+        "number": "01",
+        "publisher": "IEEE",
+        "series": "TVCG",
+        "title": "Visual Analysis and Dissemination of Scientific Literature Collections with {SurVis}",
+        "type": "article",
+        "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
+        "volume": "22",
+        "year": "2016"
+    },
+
+    "redmon2016yolo": {
+        "abstract": "Introduced the YOLO framework for unified real-time object detection using a single neural network.",
+        "author": "Redmon, Joseph and Divvala, Santosh and Girshick, Ross and Farhadi, Ali",
+        "booktitle": "Proceedings of the IEEE Conference on Computer Vision and Pattern Recognition (CVPR)",
+        "doi": "10.1109/CVPR.2016.91",
+        "keywords": "type:object_detection, yolo, cnn, computer_vision, deep_learning",
+        "pages": "779--788",
+        "series": "CVPR",
+        "title": "You Only Look Once: Unified, Real-Time Object Detection",
+        "type": "inproceedings",
+        "year": "2016"
+    },
+
+    "redmon2017yolo9000": {
+        "abstract": "Improved YOLO with joint training and hierarchical classification for detecting over 9000 object categories.",
+        "author": "Redmon, Joseph and Farhadi, Ali",
+        "booktitle": "Proceedings of the IEEE Conference on Computer Vision and Pattern Recognition (CVPR)",
+        "doi": "10.1109/CVPR.2017.690",
+        "keywords": "type:object_detection, yolo9000, cnn, computer_vision",
+        "pages": "7263--7271",
+        "series": "CVPR",
+        "title": "YOLO9000: Better, Faster, Stronger",
+        "type": "inproceedings",
+        "year": "2017"
+    },
+
+    "redmon2018yolov3": {
+        "abstract": "Presented incremental improvements to YOLO including multi-scale predictions and improved backbone architecture.",
+        "author": "Redmon, Joseph and Farhadi, Ali",
+        "doi": "10.48550/arXiv.1804.02767",
+        "journal": "arXiv preprint arXiv:1804.02767",
+        "keywords": "type:object_detection, yolov3, darknet, deep_learning",
+        "series": "arXiv",
+        "title": "YOLOv3: An Incremental Improvement",
+        "type": "article",
+        "url": "https://arxiv.org/abs/1804.02767",
+        "year": "2018"
+    },
+
+    "bochkovskiy2020yolov4": {
+        "abstract": "Introduced YOLOv4 with optimized training methods for improved object detection speed and accuracy.",
+        "author": "Bochkovskiy, Alexey and Wang, Chien-Yao and Liao, Hong-Yuan Mark",
+        "doi": "10.48550/arXiv.2004.10934",
+        "journal": "arXiv preprint arXiv:2004.10934",
+        "keywords": "type:object_detection, yolov4, cnn, deep_learning",
+        "series": "arXiv",
+        "title": "YOLOv4: Optimal Speed and Accuracy of Object Detection",
+        "type": "article",
+        "url": "https://arxiv.org/abs/2004.10934",
+        "year": "2020"
+    },
+
+    "jocher2020yolov5": {
+        "abstract": "Ultralytics implementation of YOLOv5 for real-time object detection using PyTorch.",
+        "author": "Jocher, Glenn and Chaurasia, Ayush and Qiu, Jing",
+        "doi": "10.5281/zenodo.3908559",
+        "keywords": "type:object_detection, yolov5, ultralytics, pytorch",
+        "publisher": "Zenodo",
+        "series": "Zenodo",
+        "title": "YOLO by Ultralytics",
+        "type": "software",
+        "url": "https://github.com/ultralytics/yolov5",
+        "year": "2020"
+    },
+
+    "wang2023yolov7": {
+        "abstract": "Presented YOLOv7 with trainable bag-of-freebies achieving state-of-the-art real-time detection performance.",
+        "author": "Wang, Chien-Yao and Bochkovskiy, Alexey and Liao, Hong-Yuan Mark",
+        "booktitle": "Proceedings of the IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR)",
+        "doi": "10.1109/CVPR52729.2023.00720",
+        "keywords": "type:object_detection, yolov7, cnn, real_time_detection",
+        "pages": "7464--7475",
+        "series": "CVPR",
+        "title": "YOLOv7: Trainable Bag-of-Freebies Sets New State-of-the-Art for Real-Time Object Detectors",
+        "type": "inproceedings",
+        "year": "2023"
+    },
+
+    "ultralytics2024yolov8": {
+        "abstract": "Modern YOLOv8 framework supporting detection, segmentation, classification, and tracking tasks.",
+        "author": "Ultralytics",
+        "keywords": "type:object_detection, yolov8, ultralytics, segmentation",
+        "publisher": "GitHub",
+        "series": "GitHub",
+        "title": "Ultralytics YOLOv8",
+        "type": "software",
+        "url": "https://github.com/ultralytics/ultralytics",
+        "year": "2024"
+    },
+
+    "ge2021yolox": {
+        "abstract": "Introduced YOLOX, an anchor-free version of YOLO with improved detection performance.",
+        "author": "Ge, Zheng and Liu, Songtao and Wang, Feng and Li, Zeming and Sun, Jian",
+        "doi": "10.48550/arXiv.2107.08430",
+        "journal": "arXiv preprint arXiv:2107.08430",
+        "keywords": "type:object_detection, yolox, anchor_free, deep_learning",
+        "series": "arXiv",
+        "title": "YOLOX: Exceeding YOLO Series in 2021",
+        "type": "article",
+        "url": "https://arxiv.org/abs/2107.08430",
+        "year": "2021"
+    },
+
+    "balasubramaniam2022autonomous": {
+        "abstract": "Surveyed object detection techniques and open challenges in autonomous driving systems.",
+        "author": "Balasubramaniam, A. and Pasricha, Sudeep",
+        "doi": "10.48550/arXiv.2201.07706",
+        "journal": "arXiv preprint arXiv:2201.07706",
+        "keywords": "type:autonomous_vehicles, object_detection, self_driving",
+        "series": "arXiv",
+        "title": "Object Detection in Autonomous Vehicles: Status and Open Challenges",
+        "type": "article",
+        "url": "https://arxiv.org/abs/2201.07706",
+        "year": "2022"
+    },
+
+    "sobek2023medyolo": {
+        "abstract": "Presented MedYOLO framework for medical image object detection applications.",
+        "author": "Sobek, Jakub and Medina Inojosa, Juan R. and Medina Inojosa, B. J. and Rassoulinejad-Mousavi, S. M. and Conte, Giuseppe M. and Lopez-Jimenez, Francisco and Erickson, Bradley J.",
+        "doi": "10.48550/arXiv.2312.07729",
+        "journal": "arXiv preprint arXiv:2312.07729",
+        "keywords": "type:medical_imaging, medyolo, healthcare, object_detection",
+        "series": "arXiv",
+        "title": "MedYOLO: A Medical Image Object Detection Framework",
+        "type": "article",
+        "url": "https://arxiv.org/abs/2312.07729",
+        "year": "2023"
+    },
+
+    "xu2024uavyolov8": {
+        "abstract": "Improved YOLOv8n for detecting small objects in UAV imagery.",
+        "author": "Xu, Lin and Zhao, Yulin and Zhai, Yifan and Huang, Lin and Ruan, Chunyu",
+        "doi": "10.1007/s44196-024-00632-3",
+        "journal": "International Journal of Computational Intelligence Systems",
+        "keywords": "type:uav_detection, yolov8, drone_detection, aerial_imaging",
+        "number": "1",
+        "pages": "223",
+        "publisher": "Springer",
+        "series": "IJCIS",
+        "title": "Small Object Detection in UAV Images Based on YOLOv8n",
+        "type": "article",
+        "volume": "17",
+        "year": "2024"
+    }
+
+}});
